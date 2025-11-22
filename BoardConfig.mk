4864cc0a666f78bc189c41f60379e0cdf288524d
Dede Dindin Qudsy <xtrymind@gmail.com>
2025-11-22 08:43:40 +0000

    Stone: Setup HWUI_COMPILE_FOR_PERF flag This will build framework with :
    cflags: [
        -fno-omit-frame-pointer,
        -marm,
        -mapcs,
    ]
    
    Change-Id: I887f7f91a960f579ef2ee39a534966dad3bac23f
    Signed-off-by: romgharti <romgharti@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,9 @@ TARGET_GLOBAL_THINLTO := true
 # GNSS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
+# Graphics
+HWUI_COMPILE_FOR_PERF := true
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
