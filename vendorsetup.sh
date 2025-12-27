0a61d8be54977c11151406f79a1f6768c0813413
RizkyMaulana587 <rizky.maulana.050807@gmail.com>
2025-12-27 01:33:12 +0000

    Stone: Add initial vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/AayamPokhrel/packages_apps_GameBar packages/apps/GameBar
+git clone https://github.com/mayuresh-sources/prebuilts_calyx_datura prebuilts/calyx/datura
+git clone https://github.com/mayuresh-sources/hardware_dolby -b sony-1.0 hardware/dolby
+rm -rf packages/apps/ViPER4AndroidFX && git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi
+
