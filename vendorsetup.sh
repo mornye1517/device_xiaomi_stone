61833f085b9bf86aa436ea1dc5a8f93a83b99dd8
RizkyMaulana58 <rasyid2007alkautsar@gmail.com>
2026-01-12 02:25:50 +0000

    stone: Rework vendor setup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
-git clone https://github.com/AayamPokhrel/packages_apps_GameBar packages/apps/GameBar
+git clone https://github.com/Mornye1517/kernel_xiaomi_stone -b aosp kernel/xiaomi/stone && cd kernel/xiaomi/stone && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy && cd ../../..
+git clone https://github.com/Fucking-Projekt/android_vendor_xiaomi_stone vendor/xiaomi/stone
+git clone https://github.com/Fucking-Projekt/packages_apps_GameBar packages/apps/GameBar
 git clone https://github.com/mayuresh-sources/prebuilts_calyx_datura prebuilts/calyx/datura
 git clone https://github.com/mayuresh-sources/hardware_dolby -b sony-1.0 hardware/dolby
 rm -rf packages/apps/ViPER4AndroidFX && git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
