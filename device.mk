378e2f39e7d0c30cf7fd980483b8680f2fa1cfaf
basamaryan <basam.aryan@gmail.com>
2026-01-24 19:24:37 +0530

    stone: overlay: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce

--- device.mk
+++ device.mk
@@ -289,6 +289,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayStone \
     TelephonyOverlayStone \
+    LauncherOverlayStone \
     FrameworkOverlayStone \
     SettingsOverlayStone \
     SettingsProviderOverlayStone \
