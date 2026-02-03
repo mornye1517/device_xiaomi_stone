c7568e2f41bd076a9f49ec4e4041c6b023deef9a
Mornye1517 <mornye150190@gmail.com>
2026-02-03 23:41:33 +0000

    stone: switch to mornye clang based on LLVM 23

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,7 @@ git clone https://github.com/Fucking-Projekt/android_vendor_xiaomi_stone vendor/
 git clone https://github.com/Fucking-Projekt/android_packages_apps_GameBar packages/apps/GameBar
 git clone https://github.com/mayuresh-sources/prebuilts_calyx_datura prebuilts/calyx/datura
 git clone https://github.com/mayuresh-sources/hardware_dolby -b sony-1.0 hardware/dolby
+git clone https://github.com/mornye1517/mornye_clang prebuilts/clang/host/linux-x86/clang-mornye --depth=1
 rm -rf packages/apps/ViPER4AndroidFX && git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi
 
